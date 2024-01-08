078b8a29193cbf474c18c509f9b7593bd9f3242f
David Johns <djjohns42@gmail.com>
2024-01-07 23:10:41 -0600

    add init files.

---
+++ modules/TranscodeMorseCode/__init__.py
@@ -0,0 +1,3 @@
+from modules.TranscodeMorseCode.morse_code_dict import MORSE_CODE_DICT
+from modules.TranscodeMorseCode.decrypt import decrypt
+from modules.TranscodeMorseCode.encrypt import encrypt
---
+++ modules/frames/__init__.py
@@ -0,0 +1,2 @@
+from modules.frames.TextToMorse import TextToMorse
+from modules.frames.MorseToText import MorseToText
